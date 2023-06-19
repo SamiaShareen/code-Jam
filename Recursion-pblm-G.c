fae05987d7d731c80b3b1bf0681820021cbd352e
SamiaShareen <131579645+SamiaShareen@users.noreply.github.com>
2023-06-19 08:58:47 +0600

    Recursion-G.c
    pyramid printing with recursion

---
+++ Recursion-pblm-G.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int iseven(int x){
+    return x%2==0?1:0;
+}
+int isvowel(char ch){
+    char s[]="aeiouAEIOU";
+    for(int i=0;s[i];i++){
+        if(s[i]==ch){
+            return 1;
+        }
+    }
+    return 0;
+}
+int n;
+void fun(int x){
+    int i;
+    if(x==n){
+        for(i=1;i<2*x;i++){
+            printf("*");
+        }
+        return;
+    }
+    for(i=n-1;i>=x;i--){
+        printf(" ");
+    }
+    for(i=1;i<2*x;i++){
+        printf("*");
+    }
+    printf("\n");
+    fun(x+1);
+}
+int main(){
+    scanf("%d",&n);
+    fun(1);
+    return 0;
+}
